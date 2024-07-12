7837a7a0881390aed78bebfb6c2b56801f2c3edc
cris-cmk <muthuikibara@gmail.com>
2024-07-12 04:26:52 -0500

    updated tests

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development', // Set the mode to 'development'
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -28,11 +27,4 @@ module.exports = {
       template: './index.html',
     }),
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    compress: true,
-    port: 9000,
-  },
 };
